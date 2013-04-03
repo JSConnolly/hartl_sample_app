f085cf393b0f2b1b066e268962b7caa4067bec9c
J Connolly <jsconnolly1@gmail.com>
2013-04-02 21:23:02 -0700

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,4 +10,56 @@ describe "User pages" do
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
   end
+
+  describe "profile page" do
+    let(:user){ FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+    it { should have_selector("img.gravatar")}
+  end
+
+  describe "signup" do
+    before { visit signup_path }
+
+    let(:submit){ "Create my account" }
+
+    context "with valid information" do
+      before do
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      it "has a welcome message" do
+        click_button submit
+        page.should have_selector("div.alert-success")
+      end
+    end
+
+    context "with invalid information" do
+      before do
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "user@example"
+        fill_in "Password", with: "fooba"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should not create a user" do
+        expect{ click_button submit }.not_to change(User, :count)
+      end
+      
+      it "should show errors" do
+        click_button submit
+        page.should have_selector("div.field_with_errors")
+      end
+    end
+  end
+
 end
